b4969893615e95217b0aacd16004dc714bd52ac1
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2022-03-25 21:08:38 +0100

    Non-conformant?

--- shacl/shacl.sh
+++ shacl/shacl.sh
@@ -1,1 +1,1 @@
-pyshacl -s default_sample_reduced_person_shape.ttl -f human ../samples/default_sample_reduced.jsonld
+pyshacl -s default_sample_reduced_person_shape.ttl -m -f human -df json-ld ../samples/default_sample_reduced_non_conformant.jsonld
