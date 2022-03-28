180bc5a9d72a8425bf4ceef1ad43818e254d5cf0
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2022-03-28 18:58:23 +0200

    shacl works

---
+++ samples/shacl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+pyshacl -s default_sample_reduced_person_shape.ttl -f human -df json-ld default_sample_reduced_non_conformant.jsonld
+pyshacl -s default_sample_reduced_person_shape.ttl -f human -df json-ld default_sample_reduced.jsonld
--- shacl/shacl.sh
+++
@@ -1,1 +0,0 @@
-pyshacl -s default_sample_reduced_person_shape.ttl -m -f human -df json-ld ../samples/default_sample_reduced_non_conformant.jsonld
