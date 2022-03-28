a36b486ae4ab9cb12f9bce749ed62348415f8a17
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2022-03-28 19:12:31 +0200

    sync shell script

--- samples/shacl.sh
+++ samples/shacl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
-pyshacl -s default_sample_reduced_person_shape.ttl -f human -df json-ld default_sample_reduced_non_conformant.jsonld
-pyshacl -s default_sample_reduced_person_shape.ttl -f human -df json-ld default_sample_reduced.jsonld
+pyshacl -s default_sample_reduced_person_shape.ttl -a -f human -df json-ld default_sample_reduced_non_conformant.jsonld
+pyshacl -s default_sample_reduced_person_shape.ttl -a -f human -df json-ld default_sample_reduced.jsonld
